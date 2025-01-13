52b42e104e4d6bf5e9a18dc0b55b22e790f7b70c
Omykronn <omykronn.player@gmail.com>
2025-01-13 09:28:25 +0100

    Getters & Setters

--- src/main/java/utils/Tuple.java
+++ src/main/java/utils/Tuple.java
@@ -9,12 +9,44 @@ public class Tuple {
     private int y;
 
     /**
-     * Constructeur par défaut
+     * Constructeur
      * @param x Premier entier
      * @param y Deuxième entier 
      */
     public Tuple(int x, int y) {
         this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Retourne le premier entier
+     * @return Valeur du premier entier
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Modifier le premier entier
+     * @param x Premier entier
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Retourne le deuxième entier
+     * @return Valeur du deuxième entier
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Modifier le deuxième entier
+     * @param x Deuxième entier
+     */
+    public void setY(int y) {
         this.y = y;
     }
 }
