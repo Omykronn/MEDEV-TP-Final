088735729fc0caaef5e0bec9bb2f751271a6b0ac
grigm <manon.grignon@eleves.ec-nantes.fr>
2025-01-13 09:17:52 +0100

    Test methode retourner

--- src/main/java/othello/Pion.java
+++ src/main/java/othello/Pion.java
@@ -21,6 +21,7 @@ public class Pion {
 
     /**
      * Getter Couleur
+     * @return couleur du pion (int)
      */
     public int getCouleur(){
         return this.couleur; 
@@ -34,4 +35,40 @@ public class Pion {
         this.couleur = couleur; 
     }
     
+
+    /**
+     * Retourne un pion est change sa couleur
+     */
+    public void retourner(){
+        this.setCouleur(this.couleur * -1); 
+    }
+
+    /**
+     * Renvoie la couleur du pion noir(N)/blanc(B)/vide(V)
+     * @return couleur du pion N/B/V
+     */
+    public String affichage(){
+        if (this.couleur==1){
+            return "B"; 
+        }
+        else if (this.couleur ==-1){
+            return "N"; 
+        }
+        else {
+            return "V"; 
+        }
+    }
+
+    /**
+     * Renvoie true si le pion est posé (couleur != 0), sinon false 
+     * @return pion posé ou non
+     */
+    public boolean estPose(){
+        if (this.couleur ==0){
+            return true;
+        }
+        else {
+            return false; 
+        }
+    }
 }
--- src/test/java/othello/PionTest.java
+++ src/test/java/othello/PionTest.java
@@ -1,3 +1,17 @@
+package othello; 
+
 public class PionTest {
-    
+
+
+
+    /**
+     * Test of retourner method, of class Pion.
+     */
+    @Test
+    public void testRetourner() {
+        System.out.println("retourner");
+        Pion instance = new Pion(1);
+        instance.retourner();
+        assertEquals(instance.getCouleur(), -1);
+    }
 }
