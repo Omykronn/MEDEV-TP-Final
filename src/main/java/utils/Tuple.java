fc2ff036a04dfbd7a10863a74c95bb9c4b860e4b
Omykronn <omykronn.player@gmail.com>
2025-01-13 09:25:17 +0100

    Constructeur

--- src/main/java/utils/Tuple.java
+++ src/main/java/utils/Tuple.java
@@ -1,4 +1,20 @@
+package utils;
 
-public class Tuple{
-    
+/**
+ * Tuple de deux entiers
+ * @author Simon
+ */
+public class Tuple {
+    private int x;
+    private int y;
+
+    /**
+     * Constructeur par défaut
+     * @param x Premier entier
+     * @param y Deuxième entier 
+     */
+    public Tuple(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
